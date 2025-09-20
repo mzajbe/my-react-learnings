4670bfb0f17c181311157eaa129df60e673d96fd
mzajbe <mzajbe@gmail.com>
2025-09-21 00:13:52 +0600

    learn simple routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import "./App.css";
-import { Route, Routes } from "react-router";
+import { Link, Route, Routes } from "react-router";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <h1>React Router</h1>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
       <Routes>
-        <Route path="home" element={<div>start route</div>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="about" element={<About></About>}></Route>
+        <Route path="contact" element={<Contact></Contact>}></Route>
       </Routes>
     </>
   );
