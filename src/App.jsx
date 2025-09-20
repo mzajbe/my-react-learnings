2697c08c7d19862a02e9a106f5c5096f2b06e1a7
mzajbe <mzajbe@gmail.com>
2025-09-20 23:52:20 +0600

    starting react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,15 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import AddTask from "./components/AddTask";
-import TaskList from "./components/TaskList";
-import { initialTasks } from "./data/tasks";
+import { Route, Routes } from "react-router";
 
 function App() {
-  const [tasks, setTasks] = useState(initialTasks);
-
-  const getNextId = (data) => {
-    const maxId = data.reduce(
-      (prev, current) => (prev && prev > current.id ? prev : current),
-      0
-    );
-
-    return maxId + 1;
-  };
-
-  const handleAddTask = (text) => {
-    setTasks([
-      ...tasks,
-      {
-        id: getNextId(tasks),
-        text: text,
-        done: false,
-      },
-    ]);
-  };
-
-  const handleChangeTask = (task) => {
-    const nextTasks = tasks.map((t) => {
-      if (t.id === task.id) {
-        return task;
-      } else {
-        return t;
-      }
-    });
-
-    setTasks(nextTasks);
-  };
-
-  const handleDeleteTask = (taskId) => {
-    setTasks(tasks.filter((t) => t.id !== taskId));
-  };
-
   return (
     <>
-      <h1>Prague itinerary</h1>
-      <AddTask onAdd={handleAddTask}></AddTask>
-      <TaskList
-        tasks={tasks}
-        onChangeTask={handleChangeTask}
-        onDeleteTask={handleDeleteTask}
-      ></TaskList>
+      <h1>hello</h1>
+      <Routes>
+        <Route path="home" element={<div>start route</div>}></Route>
+      </Routes>
     </>
   );
 }
